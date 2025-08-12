5b5ec13041665f141df73a7265a773e90de2cbad
ksindesign <83504612+ksindesign@users.noreply.github.com>
2025-08-13 00:44:31 +0900

    fixed my-page height

--- src/pages/MyPage/index.jsx
+++ src/pages/MyPage/index.jsx
@@ -2,7 +2,7 @@ import './index.css';
 
 const MyPage = () => {
   return (
-    <section className='container'>
+    <section className='container my-page'>
       <h2>Welcome to My Page</h2>
       <p>You have successfully logged in.</p>
     </section>
